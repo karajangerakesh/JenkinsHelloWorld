0a69860447b432f323bf11f0bb0d8075f718db9c
Rakesh Karajange <48939661+karajangerakesh@users.noreply.github.com>
2022-03-19 11:27:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("...Welcome to jenkins Training...!");
+		System.out.println("...Welcome to jenkins Training. This is test...!");
 	}
 }
